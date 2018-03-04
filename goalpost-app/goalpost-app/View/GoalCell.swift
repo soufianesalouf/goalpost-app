0c2959c2449650bd75b0c8ab6f091995a319b546
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-04 17:33:25 +0000

    end lesson 3
    Building GoalCell

---
+++ goalpost-app/goalpost-app/View/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Soufiane Salouf on 3/4/18.
+//  Copyright © 2018 Soufiane Salouf. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgress: Int){
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgress)
+    }
+
+}
