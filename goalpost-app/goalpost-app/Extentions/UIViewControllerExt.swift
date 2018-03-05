142567ba01dcb5d944793af96d921e05c493babe
Soufiane Salouf <soufiane.salouf@gmail.com>
2018-03-05 00:19:34 +0000

    end lesson 12
    Fixing dismissal of FinishGoalVC

--- goalpost-app/goalpost-app/Extentions/UIViewControllerExt.swift
+++ goalpost-app/goalpost-app/Extentions/UIViewControllerExt.swift
@@ -19,6 +19,20 @@ extension UIViewController {
         present(viewControllerPresent, animated: false, completion: nil)
     }
     
+    func presentSecondaryDetail(_ viewControllerPresent: UIViewController){
+        let transition = CATransition()
+        transition.duration = 0.3
+        transition.type = kCATransitionPush
+        transition.subtype = kCATransitionFromRight
+        
+        guard let presentedViewController = presentedViewController else { return }
+        
+        presentedViewController.dismiss(animated: false) {
+            self.view.window?.layer.add(transition, forKey: kCATransition)
+            self.present(viewControllerPresent, animated: false, completion: nil)
+        }
+    }
+    
     func dismissDetail(){
         let transition = CATransition()
         transition.duration = 0.3
--- goalpost-app/goalpost-app/Extentions/UIViewExt.swift
+++ goalpost-app/goalpost-app/Extentions/UIViewExt.swift
@@ -25,4 +25,6 @@ extension UIView {
         }, completion: nil)
     }
     
+    
+    
 }
